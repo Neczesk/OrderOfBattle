bd2c7bfccae54d363da51945f647bab3fc7c5325
Neczesk <haltermann.kyle@gmail.com>
2022-09-04 14:15:14 -0700

    implementing armylist

--- backend/armylist/armylisttest.py
+++ backend/armylist/armylisttest.py
@@ -18,15 +18,54 @@ class TestDatabaseMethods(unittest.TestCase):
 		path = "../army_lists.db"
 		conn = dbconnection.create_connection(path)
 		items_d = list_item.get_all_list_items(conn)
-		new_list_entry = ListEntry(items_d[5], 0)
+		ch: dict[int, list_item.ListItem] = {}
+		for item in items_d:
+			if items_d[item].parent == 12:
+				ch[items_d[item].id] = items_d[item]
+		self.assertEqual(len(ch), 2)
+		new_list_entry = ListEntry(items_d[5], 0, ch)
+		self.assertFalse(new_list_entry.validate())
 		child_list_entry = ListEntry(items_d[2], 1)
 		new_list_entry.add_child(child_list_entry)
+		self.assertTrue(new_list_entry.validate())
+		child_4 = ListEntry(items_d[6], 4)
+		new_list_entry.add_child(child_4)
 		self.assertTrue(new_list_entry.validate())
 		child_2 = ListEntry(items_d[2], 2)
 		child_3 = ListEntry(items_d[2], 3)
 		new_list_entry.add_child(child_2)
 		new_list_entry.add_child(child_3)
 		self.assertFalse(new_list_entry.validate())
+
+	def test_new_entry_validation(self):
+		path = "../army_lists.db"
+		conn = dbconnection.create_connection(path)
+		items_d = list_item.get_all_list_items(conn)
+		ch: dict[int, list_item.ListItem] = {}
+		for item in items_d:
+			if items_d[item].parent == 12:
+				ch[items_d[item].id] = items_d[item]
+		neon_root = ListEntry(items_d[5], 0, ch)
+		child_1 = ListEntry(items_d[2], 1)
+		child_2 = ListEntry(items_d[2], 2)
+		child_3 = ListEntry(items_d[6], 3)
+		child_4 = ListEntry(items_d[6], 4)
+		self.assertFalse(neon_root.validate())
+		neon_root.add_child(child_1)
+		self.assertTrue(neon_root.validate())
+		self.assertTrue(neon_root.test_new_entry(child_3))
+		neon_root.add_child(child_3)
+		self.assertTrue(neon_root.validate())
+		self.assertTrue(3 in neon_root.children)
+		self.assertFalse(neon_root.test_new_entry(child_4))
+		self.assertFalse(neon_root.test_and_add_entry(child_4))
+		self.assertFalse(4 in neon_root.children)
+		neon_root.remove_child(3)
+		self.assertFalse(3 in neon_root.children)
+		self.assertTrue(neon_root.test_new_entry(child_4))
+		self.assertFalse(4 in neon_root.children)
+		self.assertTrue(neon_root.test_and_add_entry(child_4))
+		self.assertTrue(4 in neon_root.children)
 
 
 if __name__ == '__main__':
--- backend/armylist/listdata/army_list.py
+++ backend/armylist/listdata/army_list.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
-
+import datetime
 import copy
 
 from .ruleset.list_item import List_Item as ListItem
+from .ruleset.ruleset import Ruleset
 
-# TODO: Update validation to take into account possible children that are not currently children of the List Entry
-#       Add possible_children as member of ListEntry sourced from the constructor
 class ListEntry:
-	def __init__(self, li: ListItem, entry_id: int):
+	def __init__(self, li: ListItem, entry_id: int, possible_children: dict[int, ListItem] = {}):
 		self.item = li
 		self.children: dict[int, ListEntry] = {}
 		self.entry_id = entry_id
+		self.possible_children = possible_children
 
 	def validate(self) -> bool:
 		for child in self.children:
@@ -23,12 +23,18 @@ class ListEntry:
 				counts[self.children[child].item.id] += 1
 			else:
 				counts[self.children[child].item.id] = 1
-		unique_items: dict[int, ListItem] = {}
-		for key in self.children:
-			if not self.children[key].item.id in unique_items:
-				unique_items[self.children[key].item.id] = self.children[key].item
+		#Key in possible children is an item that can be a child of this entry.
+		for key in self.possible_children:
+			#If key is in counts, there is at least one entry of that item and it can be checked for meeting minimums and maximums.
+			if key in counts:
+				if counts[key] < self.possible_children[key].min or counts[key] > self.possible_children[key].max:
+					return False
+			#If key is not in counts, it only needs to be checked that that possible child doesn't have a minimum, or else the list is invalid.
+			else:
+				if self.possible_children[key].min > 0:
+					return False
 		for key in counts:
-			if counts[key] < unique_items[key].min or counts[key] > unique_items[key].max:
+			if key not in self.possible_children:
 				return False
 		return True
 
@@ -51,4 +57,56 @@ class ListEntry:
 			return False
 
 class ArmyList:
-	pass
+	def __init__(self, username: str, created: datetime.datetime, modified: datetime.datetime, name: str, root: ListEntry, rules: Ruleset, items: dict[int, ListItem]):
+		self.username = username
+		self.created = created
+		self.modified = modified
+		self.name = name
+		self.root = root
+		self.rules = rules
+		self.items = items
+		self.cursor: ListEntry = self.root
+		self.id_counter: int = 1
+
+
+	def get_entry_by_id(self, entry_id) -> ListEntry:
+		pass
+
+	def validate(self) -> bool:
+		return self.root.validate()
+
+	def validate_from_cursor(self) -> bool:
+		return self.cursor.validate()
+
+	def add_item(self, new_item: ListItem):
+		new_entry = self.generate_entry(new_item)
+		self.cursor.add_child(new_entry)
+
+	def move_cursor(self, new_location: int):
+		cursor = self.get_entry_by_id(new_location)
+
+	def move_cursor_by_entry(self, new_location: ListEntry):
+		cursor = new_location
+
+	def remove_entry(self, entry_id: int, full_tree: bool = False) -> bool:
+		if full_tree:
+			return False
+		else:
+			if entry_id in self.cursor.children:
+				self.cursor.remove_child(entry_id)
+				return True
+			else:
+				return False
+
+	def get_item_possible_children(self, item_id: int) -> dict[int, ListItem]:
+		output: dict[int, ListItem] = {}
+		for key in self.items:
+			if self.items[key].parent == item_id:
+				output[key] = self.items[key]
+		return output
+
+	def generate_entry(self, new_item) -> ListEntry:
+		new_id = self.id_counter
+		self.id_counter += 1
+		pc = self.get_item_possible_children(new_item.id)
+		return ListEntry(new_item, new_id, pc)
